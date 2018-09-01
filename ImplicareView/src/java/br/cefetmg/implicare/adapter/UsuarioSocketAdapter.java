25fe04eb2a184bb84b54f5e8a607d5135c8bbadc
bryann360 <bryannbueno@hotmail.com>
2018-09-01 15:20:30 -0300

    Implementando classe UsuarioSocketsProxy e UsuarioSocketAdapter

--- ImplicareView/src/java/br/cefetmg/implicare/adapter/UsuarioSocketAdapter.java
+++ ImplicareView/src/java/br/cefetmg/implicare/adapter/UsuarioSocketAdapter.java
@@ -5,21 +5,90 @@
  */
 package br.cefetmg.implicare.adapter;
 
+import br.cefetmg.implicare.model.domain.Usuario;
+import br.cefetmg.implicare.model.exception.BusinessException;
+import br.cefetmg.implicare.model.exception.PersistenceException;
+import br.cefetmg.implicare.model.service.UsuarioManagement;
+import br.cefetmg.implicare.model.service.VagaManagement;
+import br.cefetmg.implicare.model.serviceImpl.UsuarioManagementImpl;
+import br.cefetmg.inf.implicare.util.AbstractInOut;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Gabriel
  */
 public class UsuarioSocketAdapter implements Runnable {
-
+    
+    private final Socket Socket;
+    private final UsuarioManagement UsrMan;
+    
     public UsuarioSocketAdapter(Socket socket) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.Socket = socket;
+        this.UsrMan = new UsuarioManagementImpl();
+    }
+    
+    private Socket getSocket(){
+        return Socket;
     }
 
     @Override
     public void run() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ObjectOutputStream writer;
+        ObjectInputStream reader;
+        
+        try {
+
+            writer = AbstractInOut.getObjectWriter(this.getSocket());
+            reader = AbstractInOut.getObjectReader(this.getSocket());            
+            
+            String command;
+            
+            command = reader.readUTF();
+            
+            switch(command) {
+                case "insert(Usuario)":
+                    Usuario Usuario = (Usuario) reader.readObject();            
+                    UsrMan.insert(Usuario);
+                    break;                
+                    
+                case "update(CPF_CNPJ, Usuario)":
+                    Long CPF_CNPJ = reader.readLong();
+                    
+                    Usuario = (Usuario) reader.readObject();
+                    boolean result = UsrMan.update(CPF_CNPJ, Usuario);
+                    writer.writeBoolean(result);
+                    break;
+                    
+                case "getUsuarioCod(CPF_CNPJ)": 
+                    CPF_CNPJ = reader.readLong();
+                    Usuario usr = UsrMan.getUsuarioCod(CPF_CNPJ);
+                    writer.writeObject(usr);
+                    break;
+                    
+                case "getLogin(CPF_CNPJ, Senha)":
+                    CPF_CNPJ = reader.readLong();
+                    String Senha = (String) reader.readObject();
+                    usr = UsrMan.getLogin(CPF_CNPJ, Senha);
+                    writer.writeObject(usr);
+                    break;
+            }
+            
+            writer.flush();
+            
+        } catch (IOException | ClassNotFoundException ex) {
+            Logger.getLogger(VagaSocketAdapter.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException(ex.getMessage());
+        } catch (BusinessException ex) {
+            Logger.getLogger(VagaSocketAdapter.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (PersistenceException ex) {
+            Logger.getLogger(VagaSocketAdapter.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
--- ImplicareView/src/java/br/cefetmg/implicare/proxy/UsuarioSocketProxy.java
+++ ImplicareView/src/java/br/cefetmg/implicare/proxy/UsuarioSocketProxy.java
@@ -9,31 +9,138 @@ import br.cefetmg.implicare.model.domain.Usuario;
 import br.cefetmg.implicare.model.exception.BusinessException;
 import br.cefetmg.implicare.model.exception.PersistenceException;
 import br.cefetmg.implicare.model.service.UsuarioManagement;
+import br.cefetmg.inf.implicare.util.AbstractInOut;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Gabriel
  */
 public class UsuarioSocketProxy implements UsuarioManagement{
-
+    
+    private String serverAddress;
+    private int serverPort;
+    
+    UsuarioSocketProxy(String serverAddress, int serverPort){
+        this.serverAddress = serverAddress;
+        this.serverPort = serverPort;   
+    }
+    
+    UsuarioSocketProxy(){
+        this("localhost", 2223);
+    }
+    
     @Override
     public void insert(Usuario Usuario) throws BusinessException, PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       Socket socket;
+        ObjectOutputStream writer;
+        ObjectInputStream reader;
+        
+        try {
+            socket = new Socket(this.serverAddress, this.serverPort);            
+            writer = AbstractInOut.getObjectWriter(socket);
+            reader = AbstractInOut.getObjectReader(socket);                 
+            
+            writer.writeUTF("insert(Usuario Usuario)");
+            writer.writeObject(Usuario);           
+            writer.flush();
+            
+            socket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(UsuarioSocketProxy.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException(ex.getMessage());
+        }
     }
 
     @Override
     public boolean update(Long CPF_CNPJ, Usuario Usuario) throws BusinessException, PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       boolean result;
+        Socket socket;
+        ObjectOutputStream writer;
+        ObjectInputStream reader;
+        
+        try {
+            socket = new Socket(this.serverAddress, this.serverPort);            
+            writer = AbstractInOut.getObjectWriter(socket);
+            reader = AbstractInOut.getObjectReader(socket);                 
+            
+            writer.writeUTF("update(CPF_CNPJ, Usuario)");
+            writer.writeLong(CPF_CNPJ);
+            writer.writeObject(Usuario);           
+            writer.flush();
+            
+            result = reader.readBoolean();
+            
+            socket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(UsuarioSocketProxy.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException(ex.getMessage());
+        }
+        
+        return result;
     }
 
     @Override
     public Usuario getUsuarioCod(Long CPF_CNPJ) throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Usuario Usuario; 
+        Usuario = new Usuario();
+        Socket socket;
+        ObjectOutputStream writer;
+        ObjectInputStream reader;
+        
+        try {
+            socket = new Socket(this.serverAddress, this.serverPort);            
+            writer = AbstractInOut.getObjectWriter(socket);
+            reader = AbstractInOut.getObjectReader(socket);                 
+            
+            writer.writeUTF("GetUsuarioCod(CPF_CNPJ)");
+            writer.writeLong(CPF_CNPJ);         
+            writer.flush();
+            
+    
+            
+            socket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(UsuarioSocketProxy.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException(ex.getMessage());
+        }
+        
+        return Usuario;
     }
 
     @Override
     public Usuario getLogin(Long CPF_CNPJ, String Senha) throws PersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         Usuario Usuario; 
+        Usuario = new Usuario();
+        Socket socket;
+        ObjectOutputStream writer;
+        ObjectInputStream reader;
+        
+        try {
+            socket = new Socket(this.serverAddress, this.serverPort);            
+            writer = AbstractInOut.getObjectWriter(socket);
+            reader = AbstractInOut.getObjectReader(socket);                 
+            
+            writer.writeUTF("GetLogin(CPF_CNPJ, Senha)");
+            writer.writeLong(CPF_CNPJ);
+            writer.writeUTF(Senha);
+            writer.flush();
+            
+    
+            
+            socket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(UsuarioSocketProxy.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException(ex.getMessage());
+        }
+        
+        return Usuario;
     }
+
     
 }
