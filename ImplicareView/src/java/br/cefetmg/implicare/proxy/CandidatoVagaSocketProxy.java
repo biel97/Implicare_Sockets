a635625dae891eb58db02387d011bb2aa76eff39
m2costa <matheus_mc@live.com>
2018-09-02 23:45:56 -0300

    alterações em sockets

--- ImplicareView/src/java/br/cefetmg/implicare/proxy/CandidatoVagaSocketProxy.java
+++ ImplicareView/src/java/br/cefetmg/implicare/proxy/CandidatoVagaSocketProxy.java
@@ -5,15 +5,16 @@
  */
 package br.cefetmg.implicare.proxy;
 
-import br.cefetmg.implicare.model.domain.CargoInteresse;
+import br.cefetmg.implicare.model.domain.CandidatoVaga;
 import br.cefetmg.implicare.model.exception.BusinessException;
 import br.cefetmg.implicare.model.exception.PersistenceException;
-import br.cefetmg.implicare.model.service.CargoInteresseManagement;
+import br.cefetmg.implicare.model.service.CandidatoVagaManagement;
 import br.cefetmg.inf.implicare.util.AbstractInOut;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.sql.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,22 +23,22 @@ import java.util.logging.Logger;
  *
  * @author Morato
  */
-public class CargoInteresseSocketProxy implements CargoInteresseManagement {
+public class CandidatoVagaSocketProxy implements CandidatoVagaManagement {
     
     private String serverAddress;
     private int serverPort;
     
-    public CargoInteresseSocketProxy(String serverAddress, int serverPort) {
+    public CandidatoVagaSocketProxy(String serverAddress, int serverPort) {
         this.serverAddress = serverAddress;
         this.serverPort = serverPort;        
     }
        
-    public CargoInteresseSocketProxy() {
+    public CandidatoVagaSocketProxy() {
         this("localhost", 2223);
     }
     
     @Override
-    public void insert(CargoInteresse CargoInteresse) throws BusinessException, PersistenceException {
+    public void insert(br.cefetmg.implicare.model.domain.CandidatoVaga CandidatoVaga) throws BusinessException, PersistenceException {
         Socket socket;
         ObjectOutputStream writer;
         ObjectInputStream reader;
@@ -47,20 +48,20 @@ public class CargoInteresseSocketProxy implements CargoInteresseManagement {
             writer = AbstractInOut.getObjectWriter(socket);
             reader = AbstractInOut.getObjectReader(socket);                 
             
-            writer.writeUTF("insert(CargoInteresse)");
-            writer.writeObject(CargoInteresse);           
+            writer.writeUTF("insert(CandidatoVaga)");
+            writer.writeObject(CandidatoVaga);           
             writer.flush();
             
             socket.close();
         } catch (IOException ex) {
-            Logger.getLogger(CargoInteresseSocketProxy.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(CandidatoVagaSocketProxy.class.getName()).log(Level.SEVERE, null, ex);
             throw new RuntimeException(ex.getMessage());
         }
     }
 
     @Override
-    public boolean delete(long CPF, int Cod_Cargo) throws PersistenceException {
-        Boolean result;
+    public boolean update(long CPF, int Cod_Cargo, long CNPJ, Date Dat_Publicacao, br.cefetmg.implicare.model.domain.CandidatoVaga CandidatoVaga) throws BusinessException, PersistenceException {
+        boolean result;
         Socket socket;
         ObjectOutputStream writer;
         ObjectInputStream reader;
@@ -70,24 +71,28 @@ public class CargoInteresseSocketProxy implements CargoInteresseManagement {
             writer = AbstractInOut.getObjectWriter(socket);
             reader = AbstractInOut.getObjectReader(socket);                 
             
-            writer.writeUTF("delete(CPF, Cod_Cargo)");
-            writer.writeLong(CPF); 
+            writer.writeUTF("update(CPF, Cod_Cargo, CNPJ, Dat_Publicacao, CandidatoVaga)");
+            writer.writeLong(CPF);
             writer.writeInt(Cod_Cargo);
+            writer.writeLong(CNPJ);
+            writer.writeObject((Date) Dat_Publicacao);
+            writer.writeObject(CandidatoVaga);
             writer.flush();
             
             result = reader.readBoolean();
             
             socket.close();
         } catch (IOException ex) {
-            Logger.getLogger(CargoInteresseSocketProxy.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(CandidatoVagaSocketProxy.class.getName()).log(Level.SEVERE, null, ex);
             throw new RuntimeException(ex.getMessage());
         }
+        
         return result;
     }
 
     @Override
-    public List<CargoInteresse> getCargosInteresse(long CPF) throws PersistenceException {
-        List<CargoInteresse> result;
+    public List<br.cefetmg.implicare.model.domain.CandidatoVaga> getCandidatosVaga(int Cod_Cargo, long CNPJ, Date Dat_Publicacao) throws PersistenceException {
+        List<CandidatoVaga> result;
         Socket socket;
         ObjectOutputStream writer;
         ObjectInputStream reader;
@@ -97,21 +102,23 @@ public class CargoInteresseSocketProxy implements CargoInteresseManagement {
             writer = AbstractInOut.getObjectWriter(socket);
             reader = AbstractInOut.getObjectReader(socket);                 
             
-            writer.writeUTF("getCargosInteresse(CPF)");
-            writer.writeLong(CPF);          
+            writer.writeUTF("getCandidatosVaga(Cod_Cargo, CNPJ, Dat_Publicacao)");
+            writer.writeInt(Cod_Cargo);
+            writer.writeLong(CNPJ);
+            writer.writeObject((Date) Dat_Publicacao);          
             writer.flush();
             
             result = null;
             
             try {
-                result = (List<CargoInteresse>)reader.readObject();
+                result = (List<CandidatoVaga>)reader.readObject();
             } catch (ClassNotFoundException ex) {
-                Logger.getLogger(CargoInteresseSocketProxy.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(CandidatoVagaSocketProxy.class.getName()).log(Level.SEVERE, null, ex);
             }
             
             socket.close();
         } catch (IOException ex) {
-            Logger.getLogger(CargoInteresseSocketProxy.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(CandidatoVagaSocketProxy.class.getName()).log(Level.SEVERE, null, ex);
             throw new RuntimeException(ex.getMessage());
         }
         
@@ -119,8 +126,8 @@ public class CargoInteresseSocketProxy implements CargoInteresseManagement {
     }
 
     @Override
-    public CargoInteresse getCargoInteresseCod(long CPF, int Cod_Cargo) throws PersistenceException {
-        CargoInteresse result;
+    public br.cefetmg.implicare.model.domain.CandidatoVaga getCandidatoVagaCod(long CPF, int Cod_Cargo, long CNPJ, Date Dat_Publicacao) throws PersistenceException {
+        CandidatoVaga result;
         Socket socket;
         ObjectOutputStream writer;
         ObjectInputStream reader;
@@ -130,26 +137,30 @@ public class CargoInteresseSocketProxy implements CargoInteresseManagement {
             writer = AbstractInOut.getObjectWriter(socket);
             reader = AbstractInOut.getObjectReader(socket);                 
             
-            writer.writeUTF("getCargoInteresseCod(CPF, Cod_Cargo)");
+            writer.writeUTF("getCandidatoVagaCod(CPF, Cod_Cargo, CNPJ, Dat_Publicacao)");
             writer.writeLong(CPF);
-            writer.writeInt(Cod_Cargo);          
+            writer.writeInt(Cod_Cargo);
+            writer.writeLong(CNPJ);
+            writer.writeObject((Date) Dat_Publicacao);          
             writer.flush();
             
             result = null;
             
             try {
-                result = (CargoInteresse) reader.readObject();
+                result = (CandidatoVaga) reader.readObject();
             } catch (ClassNotFoundException ex) {
-                Logger.getLogger(CargoInteresseSocketProxy.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(CandidatoVagaSocketProxy.class.getName()).log(Level.SEVERE, null, ex);
             }
             
             socket.close();
         } catch (IOException ex) {
-            Logger.getLogger(CargoInteresseSocketProxy.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(CandidatoVagaSocketProxy.class.getName()).log(Level.SEVERE, null, ex);
             throw new RuntimeException(ex.getMessage());
         }
         
         return result;
+ 
     }
+    
     
 }
